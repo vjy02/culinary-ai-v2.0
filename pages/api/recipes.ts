23fd05956829582b90c780041e5f327838c5c002
vjy02 <vyojoh@gmail.com>
2023-09-20 01:03:01 +1000

    Fixed new users My Recipes page errors

--- pages/api/recipes.ts
+++ pages/api/recipes.ts
@@ -1,35 +1,37 @@
-import { NextApiRequest, NextApiResponse } from 'next';
-import clientPromise from '../../lib/mongodb';
+import { NextApiRequest, NextApiResponse } from 'next'
+import clientPromise from '../../lib/mongodb'
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-  const client = await clientPromise;
-  const db = client.db("user_recipes");
-  const { userEmail } = req.query;
+  const client = await clientPromise
+  const db = client.db("user_recipes")
+  const { userEmail } = req.query
 
   switch (req.method) {
     case "POST":
-      const recipeObject: { title: string; content: string } = req.body;
-      console.log(recipeObject);
-
+      const recipeObject: { title: string; content: string } = req.body
+  
       const myPost = await db.collection("recipes").updateOne(
-        { email: userEmail as string },
-        {
-          $push: {
-            recipes: {
-              title: recipeObject.title,
-              content: recipeObject.content,
-            }
-          }
-        }
-      );
-
-      res.json({ status: 200, modifiedCount: myPost.modifiedCount });
-      break;
+          { email: userEmail as string },
+          {
+              $setOnInsert: { email: userEmail as string },  // Set the email on insert if the document is new
+              $push: {
+                  recipes: {
+                      title: recipeObject.title,
+                      content: recipeObject.content
+                  }
+              }
+          },
+          { upsert: true }  // This option will insert a new document if the email doesn't exist
+      )
+  
+      res.json({ status: 200, modifiedCount: myPost.modifiedCount })
+      break
+  
 
     case "GET":
-      const allPosts = await db.collection("recipes").find({ email: userEmail as string }).limit(10).toArray();
-      res.json({ status: 200, data: allPosts });
-      break;
+      const allPosts = await db.collection("recipes").find({ email: userEmail as string }).limit(10).toArray()
+      res.json({ status: 200, data: allPosts })
+      break
 
     //case "DELETE":
   }
--- pages/user-recipes.tsx
+++ pages/user-recipes.tsx
@@ -3,19 +3,33 @@ import { authOptions } from "./api/auth/[...nextauth]"
 import Layout from "../components/layout"
 
 import type { GetServerSidePropsContext } from "next"
-import type { Session } from "next-auth"
 
+type Recipe = {
+  content: string;
+}
 
 export default function ServerSidePage({ data }: { data: any }) {
+  let savedRecipes: Recipe[] | null = null
+  let noRecipes: string | null = null
 
+  if (data.data.length !== 0){
+    savedRecipes = data.data[0].recipes
+  }
+  else{
+    noRecipes = "No recipes saved! Generate a recipe and click save for it to appear"
+  }
+  console.log(savedRecipes)
   return (
     <Layout>
       <h1>User Recipes</h1>
       <pre>{JSON.stringify(data, null, 2)}</pre>
       <div>
-        {data.data[0].recipes.map((item: { content: any }) => {
+        {savedRecipes !== null ? (savedRecipes.map((item: { content: string}) => {
           return (<h3 className="whitespace-pre-wrap">{item.content}</h3>)
-        })}
+        }))
+        :
+        noRecipes
+        }
       </div>
     </Layout>
   )
@@ -23,7 +37,7 @@ export default function ServerSidePage({ data }: { data: any }) {
 export async function getServerSideProps(context: GetServerSidePropsContext) {
 
   const session = await getServerSession(context.req, context.res, authOptions)
-  // Fetch data from your API endpoint defined in db.ts
+
   if (session && session.user){
     const userEmail = session.user.email
     let res = await fetch(`http://localhost:3000/api/recipes?userEmail=${userEmail}`, {
