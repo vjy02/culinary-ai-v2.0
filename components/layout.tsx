4147661186cae831128fc179169a23e6a416b6e9
vjy02 <vyojoh@gmail.com>
2023-09-28 16:57:34 +1000

    Fixed some minor responsiveness

--- components/layout.tsx
+++ components/layout.tsx
@@ -3,7 +3,7 @@ import type { ReactNode } from "react";
 
 export default function Layout({ children }: { children: ReactNode }) {
   return (
-    <div className="md:flex md:flex-col md:justify-center md:w-[90vw] lg:m-auto">
+    <div className="md:flex md:flex-col md:justify-center md:w-[85vw] lg:m-auto">
       <Header />
       <main>{children}</main>
     </div>
