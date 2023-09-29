ff6260d6aa8903ba65968a142ba5bdb56c2e64af
vjy02 <vyojoh@gmail.com>
2023-09-30 01:07:05 +1000

    reverted openAI api handling

--- pages/api/openai.ts
+++ pages/api/openai.ts
@@ -1,57 +1,46 @@
-import { Configuration, OpenAIApi } from 'openai-edge';
-import type { NextApiRequest, NextApiResponse } from 'next';
+import type { NextApiRequest, NextApiResponse } from "next";
 
-const config = new Configuration({
-  apiKey: process.env.OPENAI_KEY,
-});
-const openai = new OpenAIApi(config);
-
-export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-  if (req.method !== 'POST') {
+export default async function handler(req: NextApiRequest, res: NextApiResponse,) {
+  if (req.method !== "POST") {
     return res.status(405).end();
   }
 
   const { ingredients, diet } = req.body;
 
-  try {
-    const response = await openai.createChatCompletion({
-      model: 'gpt-3.5-turbo',
-      stream: true,
-      messages: [
-        {
-          role: 'user',
-          content:  'Suggest 1 detailed recipe with specific quantities of each ingredients, using these ingredients:' +
-          ingredients.join(', ') +
-          `. Make sure the recipe is ${diet}, but don't name the recipe like ${diet} followed by recipe name.` +
-          `. If I gave you no ingredients, think of some random ingredients to include.
+  const APIBody = {
+    model: "gpt-3.5-turbo-0613",
+    messages: [
+    {
+        role: "user",
+        content:
+            "Suggest 1 detailed recipe with specific quantities of each ingredients, using these ingredients:" +
+            ingredients.join(", ") +
+            `. Make sure the recipe is ${diet}, but don't name the recipe like ${diet} followed by recipe name.` +
+            `. If I gave you no ingredients, think of some random ingredients to include.
                 Return with the following format: recipe: recipe name, then an empty line then Ingredients header 
-                followed by ingredient list then another empty line then numbered Instructions, separate each instruction step with an empty line. Use Australian measurements.`,
-        },
-      ],
+                followed by ingredient list then another empty line then numbered Instructions, seperate each instruction step with an empty line. Use Australian measurements.`,
+    },],
+    temperature: 0.7,
+    max_tokens: 1000,
+    top_p: 0.9,
+    frequency_penalty: -0.2,
+    presence_penalty: 0.2,
+  };
+
+  try {
+    const response = await fetch("https://api.openai.com/v1/chat/completions", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: "Bearer " + process.env.OPENAI_KEY,
+      },
+      body: JSON.stringify(APIBody),
     });
 
-    const stream = response.body; 
-    if (stream == null){
-      res.status(500).json({ error: 'Failed to fetch recipe from OpenAI' });
-      return
-    }
-    
-    const reader = stream.getReader();
-    let result = '';
-
-    while (true) {
-      const { done, value } = await reader.read();
-
-      if (done) {
-        break;
-      }
-
-      result += new TextDecoder().decode(value);
-    }
-    console.log(result)
-    res.status(200).send(result);
+    const data = await response.json();
+    res.status(200).json(data.choices[0].message.content);
   } catch (err) {
-    console.error('Failed to call OpenAI API', err);
-    res.status(500).json({ error: 'Failed to fetch recipe from OpenAI' });
+    console.error("Failed to call OpenAi API", err);
+    res.status(500).json({ error: "Failed to fetch recipe from OpenAI" });
   }
 }
--- pages/generator.tsx
+++ pages/generator.tsx
@@ -64,57 +64,28 @@ export default function GeneratorPage () {
     }
   }
 
-  async function fetchOpenApi() {
+  async function fetchOpenApi () {
     try {
-        setRecipe('');
-        setLoading(true);
-        const res = await fetch(`/api/openai`, {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({ ingredients, diet: selectedDiet })
-        });
+      setRecipe('')
+      setLoading(true)
+      const res = await fetch(`/api/openai`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ ingredients, diet: selectedDiet })
+      })
 
-        if (!res.ok || !res.body) {
-            throw new Error('Failed to fetch from server-side API');
-        }
+      if (!res.ok) {
+        throw new Error('Failed to fetch from server-side API')
+      }
 
-        let result = '';
-        const reader = res.body.getReader();
-        while (true) {
-            const { done, value } = await reader.read();
-            if (done) {
-                break;
-            }
-            const chunk = new TextDecoder().decode(value);
-            const lines = chunk.split('\n');
-
-            for (const line of lines) {
-                if (line.startsWith('data: ')) {
-                    const dataStr = line.slice(6); // Remove the "data: " prefix
-                    if (dataStr === '[DONE]') {
-                        // Stop reading when "[DONE]" is encountered
-                        setRecipe(result.trim());
-                        setLoading(false);
-                        return;
-                    }
-
-                    try {
-                        const data = JSON.parse(dataStr);
-                        if (data.choices && data.choices[0].delta && data.choices[0].delta.content) {
-                            result += data.choices[0].delta.content;
-                        }
-                    } catch (err) {
-                        console.error('Error parsing JSON chunk', err);
-                    }
-                }
-            }
-        }
+      const recipe = await res.json()
+      setRecipe(recipe)
     } catch (err) {
-        console.error('Error fetching recipe from server-side API', err);
-        setLoading(false);
+      console.error('Error fetching recipe from server-side API', err)
     }
+    setLoading(false)
   }
 
   return (
